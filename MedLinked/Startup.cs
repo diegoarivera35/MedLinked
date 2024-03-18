d94a1533a640f84b9d42f68faae169c73e14bac1
n01582292 <n01582292@NLR03110-003>
2024-03-18 14:28:46 -0400

    Add project files.

---
+++ MedLinked/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MedLinked.Startup))]
+namespace MedLinked
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
