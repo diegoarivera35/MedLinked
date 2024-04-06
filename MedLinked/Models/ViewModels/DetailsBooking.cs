82632ad092ac8b8139e5081ae482937e3ccb69d1
intel <intel@DESKTOP-OINPRHI>
2024-04-06 00:06:42 -0400

    working on Bookings CRUD

---
+++ MedLinked/Models/ViewModels/DetailsBooking.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MedLinked.Models.ViewModels
+{
+    public class DetailsBooking
+    {
+        public BookingDto SelectedBooking { get; set; }
+    }
+}
+
+
+
+
+
+
